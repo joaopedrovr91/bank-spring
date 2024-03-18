49391d67841db74bf04152236c55351294bf179b
joaopedrovr91 <joaopedrovr91@gmail.com>
2024-03-18 20:52:08 -0300

    feat: separating responsibilities from authorize

--- src/main/java/com/easy/bank/services/TransactionService.java
+++ src/main/java/com/easy/bank/services/TransactionService.java
@@ -5,15 +5,10 @@ import com.easy.bank.domain.user.User;
 import com.easy.bank.dtos.TransactionDTO;
 import com.easy.bank.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
+
 import java.time.LocalDateTime;
-import java.util.Map;
 
 
 @Service
@@ -25,7 +20,7 @@ public class TransactionService {
     private TransactionRepository repository;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authService;
 
     @Autowired
     private NotificationService notificationService;
@@ -36,7 +31,7 @@ public class TransactionService {
 
         userService.validateTransaction(sender, transaction.value());
 
-        boolean isAuthorized = this.authorizeTransaction(sender,transaction.value());
+        boolean isAuthorized = this.authService.authorizeTransaction(sender,transaction.value());
         if(!isAuthorized) {
             throw new Exception("Transação não autorizada");
         }
@@ -60,14 +55,5 @@ public class TransactionService {
         this.notificationService.sendNotification(receiver,"Transação recebida com sucesso");
 
         return  newTransaction;
-    }
-
-    public boolean authorizeTransaction (User sender, BigDecimal value) {
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
-
-        if(authorizationResponse.getStatusCode() == HttpStatus.OK ) {
-            String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado".equalsIgnoreCase(message);
-        } else return false;
     }
 }
