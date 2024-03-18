66c4afdea182f2acb5f71c169300393e998933db
joaopedrovr91 <joaopedrovr91@gmail.com>
2024-03-18 18:13:51 -0300

    feat: create User

--- src/main/java/com/easy/bank/domain/user/User.java
+++ src/main/java/com/easy/bank/domain/user/User.java
@@ -1,10 +1,8 @@
 package com.easy.bank.domain.user;
 
+import com.easy.bank.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
 
@@ -29,5 +28,15 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
 
 }
