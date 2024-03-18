d8dfe8d88f1a981d4446d30d7dbe20b393951055
joaopedrovr91 <joaopedrovr91@gmail.com>
2024-03-18 18:14:01 -0300

    feat: create UserService

---
+++ src/main/java/com/easy/bank/dtos/UserDTO.java
@@ -0,0 +1,8 @@
+package com.easy.bank.dtos;
+
+import com.easy.bank.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
